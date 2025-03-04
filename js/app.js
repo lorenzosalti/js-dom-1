0dd6a9a65b251c6100429215a2ec848106807602
lorenzosalti <lorenzosalti@gmail.com>
2025-03-04 16:22:34 +0100

    feat: completed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+
+const lampOn = document.getElementById('light-on');
+const lampOff = document.getElementById('light-off');
+const button = document.getElementById('on-off-btn');
+
+
+button.addEventListener('click', function () {
+  lampOff.className = 'hidden';
+  lampOn.className = '';
+});
